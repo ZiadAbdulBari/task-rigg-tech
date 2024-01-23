358471a9d8c49eebc42ff636bc6a085565715fa5
Ziad Abdul Bari <ziadabdulbari01@gmail.com>
2024-01-23 08:35:09 +0600

    error handle

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,10 @@
 <template>
   <main>
-    <TableWrapper :head="['Category','Brand','Price','Action']" >
+    <TableWrapper :head="['Category', 'Brand', 'Price', 'Action']">
       <TableRow v-for="product in state.products" :key="product.id">
-        <TableData type="data" :value="product.category"/>
-        <TableData type="data" :value="product.brand"/>
-        <TableData type="data" :value="product.price.toString()"/>
+        <TableData type="data" :value="product.category" />
+        <TableData type="data" :value="product.brand" />
+        <TableData type="data" :value="product.price.toString()" />
         <TableData type="action" :value="product.id.toString()" @deleteRow="removeRow" />
       </TableRow>
     </TableWrapper>
@@ -21,38 +21,48 @@ export default {
   components: {
     TableWrapper,
     TableRow,
-    TableData,
+    TableData
   },
-  emits:[],
+  emits: [],
   setup() {
     const state = reactive({
-      products:[],
+      products: []
     })
     const getProduct = () => {
-      const url = 'https://dummyjson.com/products?limit=10';
-      axios.get(url).then((response) => {
-        if(response.status==200){
-          state.products = response.data.products
-        }
-      })
+      const url = 'https://dummyjson.com/products?limit=10'
+      axios
+        .get(url)
+        .then((response) => {
+          if (response.status == 200) {
+            state.products = response.data.products
+          }
+        })
+        .catch((error) => {
+          console.log(error)
+        })
     }
     // get product id but emited the event.
     // Delete api is working. But it is not able to delete item from the list because of it is a demo api.
     // But when i hit the delete api with the product id, it returns isDeleted:true and I menually remove that product from product array.
-    const removeRow = (id)=>{
-      axios.delete(`https://dummyjson.com/products/${id}`).then((response)=>{
-        if(response.status==200){
-          const filteredProduct = state.products.filter((product)=>product.id!=id);
-          state.products=filteredProduct;
-        }
-      })
+    const removeRow = (id) => {
+      axios
+        .delete(`https://dummyjson.com/products/${id}`)
+        .then((response) => {
+          if (response.status == 200) {
+            const filteredProduct = state.products.filter((product) => product.id != id)
+            state.products = filteredProduct
+          }
+        })
+        .catch((error) => {
+          console.log(error)
+        })
     }
     onMounted(() => {
       getProduct()
     })
     return {
       state,
-      removeRow,
+      removeRow
     }
   }
 }
